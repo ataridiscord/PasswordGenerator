bdf082d19417be858a7ca06e5dc5a0803b7b3308
codexdiscord <84478811+codexdiscord@users.noreply.github.com>
2023-01-06 02:27:44 +0000

    Small update, created a variable for password length.

--- entry.cpp
+++ entry.cpp
@@ -4,10 +4,11 @@ int main()
 {
 	std::string sequence{ "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
 	std::string password{};
+	const std::uintptr_t password_length{ 16 };
 
 	std::size_t length = sequence.size();
 
-	for (int i = 0; i < 16; i++) // replace 16 with the amount of characters you'd like to generate.
+	for (int i = 0; i < password_length; i++) // replace 16 with the amount of characters you'd like to generate.
 		password += sequence[rand() % length];
 
 	std::cout << "Your password is: " << password << '\n';
